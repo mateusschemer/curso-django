c181a6b008011730d309d036fc57598e08909e91
mateusschemer <mateus.schemer@gmail.com>
2022-07-12 06:14:39 -0300

    acrescentado requests na def home

--- pyschemer/base/views.py
+++ pyschemer/base/views.py
@@ -2,6 +2,6 @@ from django.http import HttpResponse
 from django.shortcuts import render
 # Create your views here.
 
-def home(request):
+def home(requests):
     return HttpResponse('Olá Django')
 
