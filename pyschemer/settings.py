b74f3702a4a4dbe67c6ebc1de4c20fac93b7cd7f
mateusschemer <mateus.schemer@gmail.com>
2022-07-13 17:03:25 -0300

    conf DEBUG

--- pyschemer/settings.py
+++ pyschemer/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,7 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-xai0z*0eb8a$nm7juut$zs$p-dyqhyu8f9s=!l0in7xg9$$5nj'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+
+
+DEBUG = config('DEBUG', cast= bool)
 
 ALLOWED_HOSTS = ['*']
 
