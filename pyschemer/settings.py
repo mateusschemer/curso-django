eb4329ebf207cf339d4fa45e7664dcfb652d112a
mateusschemer <mateus.schemer@gmail.com>
2022-07-14 14:15:17 -0300

    conf SECRET_KEY

--- pyschemer/settings.py
+++ pyschemer/settings.py
@@ -25,11 +25,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-
+from decouple import Csv
 
 DEBUG = config('DEBUG', cast= bool)
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast= Csv())
+
 
 
 # Application definition
